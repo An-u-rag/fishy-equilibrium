901804d9bf6bb749d6e2b04270f87cfcd6bef27a
AdamWarlord <anuragparcha315@gmail.com>
2021-07-07 14:02:47 +0530

    Added backend api connection

--- client-react/src/app.js
+++ client-react/src/app.js
@@ -6,11 +6,19 @@ import Join from './components/join/join'
 import Game from './components/game/game'
 
 
-const App = () => (
+const App = () => {
+    const [data, setData] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch("/api").then(res => res.json()).then(data => setData(data.message))
+    }, [])
+    return(
     <Router>
+        <code>{!data ? "Not connected to backend" : data}</code>
         <Route path="/" exact component={Join} />
         <Route path="/game" component={Game} />
     </Router>
-)
+    )
+}
 
 export default App
--- server-for-react/app.js
+++ server-for-react/app.js
@@ -18,6 +18,10 @@ app.use(express.static(__dirname + '/client/public'))
 
 const PORT = process.env.PORT || 5000
 
+app.get('/api', (req,res) => {
+    res.json({ message: "Backend Connected" });
+})
+
 app.use(router)
 
 // app.get('/', (req, res) => {
